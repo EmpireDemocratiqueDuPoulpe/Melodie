b5acd8c40e0edd2c7820adb4c4cf94bff3a2faad
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-02-02 23:06:17 +0100

    Initial commit

---
+++ Melodie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Melodie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
