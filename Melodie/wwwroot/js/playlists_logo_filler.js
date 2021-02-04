cae5df166675296bbce762d38c008bffb3dd8659
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-02-04 16:36:52 +0100

    Added style for most of the playlist container

---
+++ Melodie/wwwroot/js/playlists_logo_filler.js
@@ -0,0 +1,18 @@
+﻿/** Custom foreach **/
+const forEach = function (array, callback, scope) {
+    for (let i = 0; i < array.length; i++) {
+        callback.call(scope, i, array[i]);
+    }
+}
+
+/** Set playlists logo text **/
+const playlists = document.querySelectorAll(".playlist-block");
+
+forEach(playlists, function (index, playlist) {
+   const logo = playlist.querySelector(".playlist-logo");
+   const title = playlist.querySelector(".playlist-title");
+   
+   const acronym = title.textContent.match(/\b(\w)/g).join("").toUpperCase();
+   
+   logo.setAttribute("data-acronym", acronym);
+});
