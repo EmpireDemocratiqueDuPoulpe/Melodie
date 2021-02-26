7c439b07aa09c27b6ab3ab48a9d3ea9d3a7e63ee
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-02-26 20:20:26 +0100

    Fixed a bug where the playlist logo acronym didn't change after an update

--- Melodie/wwwroot/js/PlaylistsLogoFiller.js
+++ Melodie/wwwroot/js/PlaylistsLogoFiller.js
@@ -1,36 +1,69 @@
-﻿/** Get playlists **/
-const smallPlaylists = document.querySelectorAll(".playlist-block");
-const fullPlaylist = document.querySelectorAll("#playlist-info");
+﻿/* Const
+-------------------------------------------------- */
 
-let playlists;
-
-if (smallPlaylists != null && fullPlaylist != null) {
-    playlists = [...smallPlaylists, ...fullPlaylist];
-} else {
-    if (smallPlaylists == null) {
-        playlists = fullPlaylist;
-    } else {
-        playlists = smallPlaylists;
+class PlaylistLogo {
+    logo = null;
+    titleContainer = null;
+    acronym = "";
+    
+    constructor(logo, titleContainer) {
+        this.logo = logo;
+        this.titleContainer = titleContainer;
+    }
+    
+    isTitleContainerAnInput() {
+        if (this.titleContainer == null) return false;
+        
+        return this.titleContainer.tagName.toLowerCase() === "input" ||
+                this.titleContainer.tagName.toLowerCase() === "textarea";
+    }
+    
+    getTitle() {
+        if (this.titleContainer == null) return "";
+        let title;
+        
+        if (this.isTitleContainerAnInput()) {
+            title = this.titleContainer.value;
+        } else {
+            title = this.titleContainer.textContent;
+        }
+        
+        return title;
+    }
+    
+    buildAcronym(title) {
+        this.acronym = title.match(/\b(\w)/g).join("").toUpperCase();
+    }
+    
+    setAcronym() {
+        if (this.logo == null) return;
+        
+        this.buildAcronym(this.getTitle());
+        this.logo.setAttribute("data-acronym", this.acronym);
     }
 }
 
-/** Set playlists logo text **/
-forEach(playlists, function (index, playlist) {
-   const logo = playlist.querySelector(".playlist-logo.auto-acronym");
-   const title = playlist.querySelector(".playlist-title");
-   let acronym;
-   
-   if (logo != null) {
-       if (title.tagName.toLowerCase() === "input") {
-           acronym = title.value;
-       } else {
-           acronym = title.textContent;
-       }
+const logos = [];
 
-       if (acronym != null) {
-           acronym = acronym.match(/\b(\w)/g).join("").toUpperCase();
+/* Set all acronyms
+-------------------------------------------------- */
 
-           logo.setAttribute("data-acronym", acronym);
-       }
-   }
+document.addEventListener('DOMContentLoaded', function(event) {
+    const playlists = document.querySelectorAll(".playlist-block, #playlist-info") || [];
+
+    forEach(playlists, function (index, playlist) {
+        const logo = playlist.querySelector(".playlist-logo.auto-acronym");
+        const titleContainer = playlist.querySelector(".playlist-title");
+
+        if (logo != null) {
+            logos[index] = new PlaylistLogo(logo, titleContainer);
+            logos[index].setAcronym();
+
+            if (logos[index].isTitleContainerAnInput()) {
+                logos[index].titleContainer.addEventListener('change', function () {
+                    logos[index].setAcronym()
+                }, false);
+            }
+        }
+    });
 });
